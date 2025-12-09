dc2ac807c3b13ab807970fd7dd4dad348a537c87
El Pancho <w00kyx@gmail.com>
2025-12-09 11:16:09 -0300

    operadores lógicos

---
+++ OperadoresLogicos/OperadoresLogicos/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Operadores lógicos!");
+
+
+bool valor1 = true, valor2 = true, valor3 = true;
+
+//AND
+bool resultAnd = valor1 && !valor2 && valor3;
+Console.WriteLine($"resultAnd : {resultAnd}");
+
+//OR
+bool resultOr = valor1 || !valor2 || valor3;
+Console.WriteLine($"resultOr : {resultOr}");
