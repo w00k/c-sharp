0beede6a506550cbdcfa59d25360fe999e733a71
El Pancho <w00kyx@gmail.com>
2025-12-09 16:29:02 -0300

    switch y ciclos

--- SwitchExample/Program.cs
+++ SwitchExample/Program.cs
@@ -11,7 +11,33 @@ switch (switchControl)
     case "21":
         Console.WriteLine("Black Jack!!!");
         break;
-    default: 
+    default:
         Console.WriteLine("Valor ingresado no válido");
         break;
+}
+
+Random generadorAleatorio = new Random();
+int carta = generadorAleatorio.Next(1, 13); // Genera un número entre 1 y 12
+
+Console.WriteLine(carta);
+
+while (carta < 10)
+{
+    Console.WriteLine($" while - Carta: {carta}");
+    carta = generadorAleatorio.Next(1, 13);
+}
+Console.WriteLine($" while - Carta: {carta}");
+
+
+do
+{
+    Console.WriteLine($"do-while - Carta: {carta}");
+    carta = generadorAleatorio.Next(1, 13);
+} while (carta > 10);
+
+Console.WriteLine($"do-while - Carta: {carta}");
+
+for (int i = 0; i < 20; i++)
+{
+    Console.WriteLine($"i: {i}");
 }
